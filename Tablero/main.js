e7530dd43321aeb36cabea70ff3059a45ea8a794
Leydy Moreno <leydy.moreno@comafi.com.ar>
2021-03-09 23:18:07 -0300

    feat: agregar intercambio de elementos

--- Tablero/main.js
+++ Tablero/main.js
@@ -6,6 +6,9 @@ let level = 4;
 let contTime = 0;
 let process;
 let timeJuego = 30;
+let tabMirror = [];
+
+let positionSelected = []; //origen, destino
 
 const controTiempo = () => {
   clearInterval(process);
@@ -30,34 +33,24 @@ const finJuego = () => {
 const createTablero = (level) => {
   tab.innerHTML = "";
   for (let i = 0; i < level; i++) {
+    tabMirror[i] = [];
     for (let j = 0; j < level; j++) {
       const newDiv = document.createElement("p");
       newDiv.setAttribute("id", `${i}-${j}`);
-      newDiv.innerHTML = items[getRandomInt(0, 7)];
+      let value = items[getRandomInt(0, 7)];
+      tabMirror[i][j] = value;
+      newDiv.innerHTML = value;
       tab.appendChild(newDiv);
     }
   }
-
-  getElement();
+  addEventClickElement();
 };
 
 function getRandomInt(min, max) {
-  // console.log(`ramdom: `, ramdom);
-  // console.log(`ramdom con floor`, Math.floor(ramdom));
-  // console.log(`ramdom*(max-min)`, ramdom * (max - min));
-  // console.log(`ramdom con floor y max y min`, Math.floor(ramdom * (max - min)));
-  // console.log(
-  //   `ramdom con floor max min y + min`,
-  //   Math.floor(ramdom * (max - min)) + min
-  // );
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const changeLevel = () => {
-  // level += 1;
-  // console.log("soy level", level);
-  // createTablero(level);
-  // tab.style.width = `${50 * level}px`;
   if (level < 15) {
     level += 1;
   } else {
@@ -67,16 +60,64 @@ const changeLevel = () => {
   tab.style.width = `${50 * level}px`;
 };
 
-const getElement = () => {
+const addEventClickElement = () => {
   const elemt = document.getElementsByTagName("p");
+
   console.log(elemt);
   for (let i = 0; i < elemt.length; i++) {
     elemt[i].addEventListener("click", (elem) => {
-      console.log(elem);
+      if (positionSelected.length > 1) {
+        positionSelected = [];
+      }
+      positionSelected.push(elem.target.id);
+      if (positionSelected.length === 2) {
+        console.log(`posiciones:${positionSelected}`);
+        intercambiarElementos();
+      }
     });
   }
 };
 
+const intercambiarElementos = () => {
+  let origen, destino;
+  if (isAdyacente()) {
+    contentOrigen = document.getElementById(positionSelected[0]).textContent;
+    contentDestino = document.getElementById(positionSelected[1]).textContent;
+
+    origen = document.getElementById(positionSelected[0]);
+    destino = document.getElementById(positionSelected[1]);
+
+    destino.innerText = contentOrigen;
+    origen.innerText = contentDestino;
+
+    console.log("Soy adyacente");
+  } else {
+    console.log("No soy adyacente");
+  }
+};
+
+const isAdyacente = () => {
+  const pos = positionSelected[0].split("-"); //origen
+  const x = pos[0];
+  const y = pos[1];
+  console.log(x, y);
+  console.log(`${positionSelected[0]} posibles movimientos: 
+  ${Number(x) - 1}-${y}, 
+  ${Number(x) + 1}-${y},
+  ${x}-${Number(y) - 1},
+  ${x}-${Number(y) + 1}
+  `);
+  if (
+    positionSelected[1] === `${Number(x) - 1}-${y}` || //arriba
+    positionSelected[1] === `${Number(x) + 1}-${y}` || //abajo
+    positionSelected[1] === `${x}-${Number(y) - 1}` || //izquierda
+    positionSelected[1] === `${x}-${Number(y) + 1}` //derecha
+  ) {
+    return true;
+  }
+  return false;
+};
+
 //inicio de juego
-controTiempo();
+// controTiempo();
 createTablero(level);
