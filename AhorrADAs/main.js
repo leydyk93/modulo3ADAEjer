2a56e4a1ae9c1043e8c0f0286031947935570129
leydyk <leydy.moreno@comafi.com.ar>
2021-04-29 22:38:07 -0300

    feat: agregar reportes de totales y resumen

--- AhorrADAs/main.js
+++ AhorrADAs/main.js
@@ -5,14 +5,20 @@ const categoriesEdit = document.getElementById("edit-op-categories");
 const listCategories = document.getElementById("categories-list");
 const inputNewCategory = document.getElementById("category-input-new");
 
-const formNewOperation = document.querySelectorAll(
+const formElementsNewOperation = document.querySelectorAll(
   "#new-operation input[data-owner], #new-operation select[data-owner]"
 );
+const formNewOperation = document.getElementById("form-new-operation");
 const listOperations = document.getElementById("operations");
 
-const formEditOperation = document
+const formElementsEditOperation = document
   .querySelector("#edit-operation")
   .querySelectorAll("input[data-owner], select[data-owner]");
+
+const totalesCategoryReport = document.getElementById(
+  "report-total-categories"
+);
+const reportResumen = document.getElementById("report-resumen");
 
 console.log(sections, "Objecto");
 const sectionsList = [...sections];
@@ -45,6 +51,11 @@ const categories = [
 
 let operations = [];
 let operationEditar = {};
+let reportsSections = {
+  resumen: [],
+  totalesCategory: [],
+  totalesMes: [],
+};
 
 // categories operations
 const addCategories = () => {
@@ -116,13 +127,24 @@ const setValueCategoriesSelect = () => {
 };
 
 //Operaciones
+const showFormNewOperation = () => {
+  formNewOperation.reset();
+  controlVisibility("new-operation");
+};
+
 const addOperation = () => {
   let operation = {};
-  console.log(formNewOperation);
-  for (let i = 0; i < formNewOperation.length; i++) {
+  console.log("Este es form new operation", formElementsNewOperation);
+  for (let i = 0; i < formElementsNewOperation.length; i++) {
     operation["id"] = generateId();
-    operation[formNewOperation[i].getAttribute("name")] =
-      formNewOperation[i].value;
+    console.log(
+      "nombre de la propiedad: ",
+      formElementsNewOperation[i].getAttribute("name"),
+      "valor",
+      formElementsNewOperation[i].value
+    );
+    operation[formElementsNewOperation[i].getAttribute("name")] =
+      formElementsNewOperation[i].value;
   }
   console.log(operation);
   operations.push(operation);
@@ -190,12 +212,12 @@ const editOperation = (id) => {
   console.log(id, "Quiero editar");
   operationEditar = operations.find((operation) => operation.id === id);
   console.log(operationEditar, "item a editar");
-  for (let i = 0; i < formEditOperation.length; i++) {
-    formEditOperation[i].value =
-      operationEditar[formEditOperation[i].getAttribute("name")];
+  for (let i = 0; i < formElementsEditOperation.length; i++) {
+    formElementsEditOperation[i].value =
+      operationEditar[formElementsEditOperation[i].getAttribute("name")];
     // console.log(
-    //   formEditOperation[i].getAttribute("name"),
-    //   operationEditar[formEditOperation[i].getAttribute("name")]
+    //   formElementsEditOperation[i].getAttribute("name"),
+    //   operationEditar[formElementsEditOperation[i].getAttribute("name")]
     // );
   }
 };
@@ -204,9 +226,9 @@ const confirmEditOperation = () => {
   // let operation = {};
   //if (Object.keys(operationEditar).length !== 0) {
   //console.log(operationEditar);
-  for (let i = 0; i < formEditOperation.length; i++) {
-    operationEditar[formEditOperation[i].getAttribute("name")] =
-      formEditOperation[i].value;
+  for (let i = 0; i < formElementsEditOperation.length; i++) {
+    operationEditar[formElementsEditOperation[i].getAttribute("name")] =
+      formElementsEditOperation[i].value;
   }
 
   const posOperation = operations.findIndex((e) => e.id === operationEditar.id);
@@ -220,6 +242,93 @@ const confirmEditOperation = () => {
 
 const cancelar = () => {
   controlVisibility("home");
+};
+
+//reports
+generateReports = () => {
+  controlVisibility("reports");
+  let totalReportCategory = [];
+  categories.forEach((category) => {
+    let itemReport = {
+      category: category.name,
+      ganancia: 0,
+      gasto: 0,
+      balance: 0,
+    };
+    operations.forEach((operation) => {
+      if (category.name === operation.category) {
+        if (operation.type === "Gasto") {
+          itemReport.gasto += parseFloat(operation.monto);
+        }
+        if (operation.type === "Ganancia") {
+          itemReport.ganancia += parseFloat(operation.monto);
+        }
+      }
+    });
+    itemReport.balance = itemReport.ganancia - itemReport.gasto;
+    totalReportCategory.push(itemReport);
+  });
+  reportsSections.totalesCategory = totalReportCategory;
+  console.log(totalReportCategory);
+
+  let maxGanancia = getMaximosCategory("ganancia");
+  let maxGasto = getMaximosCategory("gasto");
+  let maxBalance = getMaximosCategory("balance");
+
+  reportsSections.resumen.push({
+    title: "Categoría con mayor ganancia",
+    category: maxGanancia.category,
+    monto: maxGanancia.ganancia,
+  });
+  reportsSections.resumen.push({
+    title: "Categoría con mayor gasto",
+    category: maxGasto.category,
+    monto: maxGasto.gasto,
+  });
+  reportsSections.resumen.push({
+    title: "Categoría con mayor balance",
+    category: maxBalance.category,
+    monto: maxBalance.balance,
+  });
+  console.log(reportsSections.resumen);
+  paintReports();
+};
+
+const getMaximosCategory = (campo) => {
+  return reportsSections.totalesCategory.reduce((prev, current) =>
+    prev[campo] > current[campo] ? prev : current
+  );
+};
+
+paintReports = () => {
+  totalesCategoryReport.innerHTML = "";
+  reportsSections.totalesCategory.forEach((category) => {
+    let node = document.createElement("div");
+    node.innerHTML = `
+    <div class="columns has-text-weight-medium is-mobile">
+    <div class="column">${category.category}</div>
+    <div class="column">${category.ganancia}</div>
+    <div class="column">${category.gasto}</div>
+    <div class="column">${category.balance}</div>
+  </div>
+  `;
+    totalesCategoryReport.appendChild(node);
+  });
+
+  reportResumen.innerHTML = "";
+  reportsSections.resumen.forEach((resumen) => {
+    let node = document.createElement("div");
+    node.innerHTML = `
+    <div class="columns has-text-weight-medium is-mobile">
+      <div class="column">${resumen.title}</div>
+      <div class="column">
+        <span class="tag is-info is-light is-medium">${resumen.category}</span>
+      </div>
+      <div class="column">${resumen.monto}</div>
+    </div>
+  `;
+    reportResumen.appendChild(node);
+  });
 };
 
 //local Storage
